fffa3275c512ef2cde4f29dcb8f8dae130634127
Yannick Schurter <yannick.schurter@gmail.com>
2016-07-27 14:01:51 +0200

    use chrome.management to find the app ID, fix #3

--- extension/popup.js
+++ extension/popup.js
@@ -1,7 +1,27 @@
-const appId = 'cgkdokmipoadhnjmckgkmgeffllhhcna';
-
 window.onload = function () {
-  document.getElementById('launch-button').addEventListener('click', function launchApp() {
-    chrome.runtime.sendMessage(appId, { wakeUp: true });
+  const appName      = 'ezpaarse logger (app)';
+  const installText  = document.getElementById('install');
+  const launchButton = document.getElementById('launch-button')
+
+  chrome.management.getAll(extensions => {
+    const ext = extensions.find(e => (e.name.toLowerCase() === appName));
+
+    if (!ext) {
+      launchButton.style.display = 'none';
+      installText.style.display  = 'block';
+      return;
+    }
+
+    launchButton.addEventListener('click', function launchApp() {
+      if (ext.enabled) {
+        return chrome.management.launchApp(ext.id);
+      }
+
+      if (confirm('The application is disabled. Do you want to enable it ?')) {
+        chrome.management.setEnabled(ext.id, true, () => {
+          chrome.management.launchApp(ext.id);
+        });
+      }
+    });
   });
 };
