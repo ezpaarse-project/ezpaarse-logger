b8bf5922a0d73ea221c6b597820a1982e320b0fe
Yannick Schurter <yannick.schurter@gmail.com>
2016-09-29 17:19:37 +0200

    make extension toggleable

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,18 @@
 const ports = new Set();
 const tabs  = new Set();
 
-chrome.webRequest.onCompleted.addListener(details => {
-  if (tabs.has(details.tabId)) { return; }
-
-  ports.forEach(port => port.postMessage(JSON.stringify(details)));
-}, { urls: ['<all_urls>'] }, ['responseHeaders']);
+const icons = {
+  enabled: {
+    16: 'img/ezlogger-16.png',
+    48: 'img/ezlogger-48.png',
+    128: 'img/ezlogger-128.png'
+  },
+  disabled: {
+    16: 'img/ezlogger-disabled-16.png',
+    48: 'img/ezlogger-disabled-48.png',
+    128: 'img/ezlogger-disabled-128.png'
+  }
+};
 
 // long-lived connection
 chrome.runtime.onConnect.addListener(port => {
@@ -21,3 +28,59 @@ chrome.runtime.onConnect.addListener(port => {
     if (tabId) { tabs.delete(tabId); }
   });
 });
+
+chrome.storage.local.get('activated', item => {
+  const activated = item && item.activated;
+  setActivated(activated === undefined ? true : activated);
+});
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request.getState) {
+    sendState();
+  } else if (request.setState) {
+    setActivated(request.setState.activated);
+  }
+});
+
+/**
+ * Enable or disable the extension by adding/removing the network listener
+ * @param {Boolean} activated
+ */
+function setActivated(activated) {
+  activated = !!activated;
+
+  chrome.browserAction.setIcon({ path: activated ? icons.enabled : icons.disabled });
+
+  if (!activated) {
+    chrome.webRequest.onCompleted.removeListener(networkListener);
+  } else if (!isActivated()) {
+    chrome.webRequest.onCompleted.addListener(networkListener, { urls: ['*://*/*'] }, ['responseHeaders']);
+  }
+
+  chrome.storage.local.set({ activated: activated });
+  sendState();
+}
+
+/**
+ * Notify the popup about the current state
+ */
+function sendState() {
+  chrome.runtime.sendMessage({ stateChange: { activated: isActivated() } });
+}
+
+/**
+ * Check whether the network is being listened or not
+ * @return {Boolean}
+ */
+function isActivated() {
+  return chrome.webRequest.onCompleted.hasListener(networkListener);
+}
+
+/**
+ * Network listener, send request details to the web app
+ * @param  {Object} details  request details
+ */
+function networkListener(details) {
+  if (tabs.has(details.tabId)) { return; }
+  ports.forEach(port => port.postMessage(JSON.stringify(details)));
+}
